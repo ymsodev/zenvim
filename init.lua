939d8a029caa0cc1b3b700961b84b6900ba8dddc
ymsodev <ymsodev@gmail.com>
2023-11-24 15:43:45 -0800

    Update

--- init.lua
+++ init.lua
@@ -71,10 +71,14 @@ require('lazy').setup({
   {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
     opts = {
       char = '┊',
       show_trailing_blankline_indent = false,
     },
+    config = function()
+      require("ibl").setup()
+    end
   },
 
   {
