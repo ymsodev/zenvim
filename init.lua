91c2b757229c40f18cc7a1c3fa9a3aa87d9936e3
ymsodev <ymsodev@gmail.com>
2023-11-30 23:22:27 -0800

    minor changes; add bufferline

--- init.lua
+++ init.lua
@@ -2,6 +2,33 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Make line numbers default
+vim.wo.number = true
+
+-- Enable break indent
+vim.o.breakindent = true
+
+vim.o.termguicolors = true
+
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', 'v:count == 0 ? "gk" : "k"', { expr = true, silent = true })
+vim.keymap.set('n', 'j', 'v:count == 0 ? "gj" : "j"', { expr = true, silent = true })
+
+-- Highlight on yank
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
+-- Open file explorer
+vim.keymap.set('n', '<leader>fe', vim.cmd.Ex)
+
 -- Install Lazy package manager
 local lazy_path = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazy_path) then
@@ -17,6 +44,18 @@ end
 vim.opt.rtp:prepend(lazy_path)
 
 require('lazy').setup({
+  {
+    -- Catppuccin theme
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    config = function()
+      require('catppuccin').setup({
+        flavour = 'macchiato',
+      })
+      vim.cmd.colorscheme('catppuccin')
+    end
+  },
+
   {
     -- Detect tabstop and shiftwidth automatically
     'tpope/vim-sleuth',
@@ -43,18 +82,27 @@ require('lazy').setup({
   },
 
   {
-    -- Catppuccin theme
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000,
-    config = function()
-      require('catppuccin').setup({
-        flavour = 'macchiato',
-      })
-      vim.cmd.colorscheme('catppuccin')
-    end
+    -- Add support for file icons 
+    'nvim-tree/nvim-web-devicons',
   },
 
+  {
+    -- Add a buffer line (tabs)
+    'akinsho/bufferline.nvim',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    keys = {
+      { '<leader>br', '<Cmd>BufferLineCloseRight<CR>', desc = 'Delete buffers to the right' },
+      { '<leader>bl', '<Cmd>BufferLineCloseLeft<CR>', desc = 'Delete buffers to the left' },
+      { '[b', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev buffer' },
+      { ']b', '<cmd>BufferLineCycleNext<cr>', desc = 'Next buffer' },
+    },
+    config = function()
+      require('bufferline').setup()
+    end,
+  },
+  
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
@@ -77,7 +125,7 @@ require('lazy').setup({
       show_trailing_blankline_indent = false,
     },
     config = function()
-      require("ibl").setup()
+      require('ibl').setup()
     end
   },
 
@@ -88,43 +136,15 @@ require('lazy').setup({
   },
 
   {
-    -- Add terminal
+    -- Toggle terminal
     'akinsho/toggleterm.nvim',
     config = function()
-      require('toggleterm').setup {
+      require('toggleterm').setup({
         open_mapping = '<C-t>',
-        direction = 'float',
-        float_opts = {
-          border = 'curved',
-        },
-      }
+        direction = 'horizontal',
+        size = 15,
+      })
     end,
   },
 })
 
--- Make line numbers default
-vim.wo.number = true
-
--- Enable break indent
-vim.o.breakindent = true
-
-vim.o.termguicolors = true
-
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
--- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
--- Highlight on yank
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
-})
-
--- Open file explorer
-vim.keymap.set('n', '<leader>fe', vim.cmd.Ex)
